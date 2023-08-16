56277c0bfe9ec051e19411def21071a3586ae7a9
Abhishek Yeole <>
2023-08-16 16:37:23 +0530

    changes in font style

--- src/components/CoverPage3/SecondSection.jsx
+++ src/components/CoverPage3/SecondSection.jsx
@@ -18,270 +18,316 @@ function SecondSection() {
     });
 
 
-    return (
-
-
-        mediaQueries.mobile ?
-        <div className="CoverPage3TextBoxRow2">
-            <Carousel>
-
-                <Carousel.Item>
-
-                    <div className="TextBoxRow2Col">
-
-                        <Image className="Row2Col1Img" src={Row2Col1Img} alt="img" />  
-
-                        <div className="Row2ColfloatImg">
-                            <h6>Sale</h6>
-                        </div>
-
-                        <div className="Row2ColFloatBtnBox">
-
-                            <div className="Row2ColFloatBtn">
-                                <Image className="FloatBtnIcon" src={FavoriteIcon} alt="favorite icon" />
-                            </div>
-
-                            <div className="Row2ColFloatBtn">
-                                <Image className="FloatBtnIcon" src={BucketIcon} alt="bucket icon" />
-                            </div>
-
-                            <div className="Row2ColFloatBtn">
-                                <Image className="FloatBtnIcon" src={ViewIcon} alt="view icon" />
-                            </div>
-
-                        </div>
-
-                        <CoverPage3Card
-                            row1Text="Expert instruction"
-                            row2Text = "Our Experts Teacher"
-                        />
-                    </div>
-
-                </Carousel.Item>
-                
-                <Carousel.Item>
-                
-                    <div className="TextBoxRow2Col">
-                        <Image className="Row2Col1Img" src={Row2Col2Img} alt="img" />
-                        
-                        <div className="Row2ColfloatImg">
-                            <h6>Sale</h6>
-                        </div>
-
-                        <div className="Row2ColFloatBtnBox">
-
-                            <div className="Row2ColFloatBtn">
-                                <Image className="FloatBtnIcon" src={FavoriteIcon} alt="favorite icon" />
-                            </div>
-
-                            <div className="Row2ColFloatBtn">
-                                <Image className="FloatBtnIcon" src={BucketIcon} alt="bucket icon" />
-                            </div>
-
-                            <div className="Row2ColFloatBtn">
-                                <Image className="FloatBtnIcon" src={ViewIcon} alt="view icon" />
-                            </div>
-
-                        </div>
-
-                        <CoverPage3Card
-                            row1Text="27 OnlineCourses"
-                            row2Text = "Get Quality Education"
-                        />
-                    </div>
-
-                </Carousel.Item>
-                
-                <Carousel.Item>
-                
-                    <div className="TextBoxRow2Col">
-
-                        <Image className="Row2Col1Img" src={Row2Col3Img} alt="img" />
-                        
-                        <div className="Row2ColfloatImg">
-                            <h6>Sale</h6>
-                        </div>
-
-                        <div className="Row2ColFloatBtnBox">
-
-                            <div className="Row2ColFloatBtn">
-                                <Image className="FloatBtnIcon" src={FavoriteIcon} alt="favorite icon" />
-                            </div>
-
-                            <div className="Row2ColFloatBtn">
-                                <Image className="FloatBtnIcon" src={BucketIcon} alt="bucket icon" />
-                            </div>
-
-                            <div className="Row2ColFloatBtn">
-                                <Image className="FloatBtnIcon" src={ViewIcon} alt="view icon" />
-                            </div>
-
-                        </div>
-
-                        <CoverPage3Card
-                            row1Text = "training Courses"
-                            row2Text = "Our Popular Courses"
-                        />
-                    </div>
-
-                </Carousel.Item>
-                
-                <Carousel.Item>
-                
-                    <div className="TextBoxRow2Col">
-                        <Image className="Row2Col1Img" src={Row2Col4Img} alt="img" />
-                        
-                        <div className="Row2ColfloatImg">
-                            <h6>Sale</h6>
-                        </div>
-
-                        <div className="Row2ColFloatBtnBox">
-
-                            <div className="Row2ColFloatBtn">
-                                <Image className="FloatBtnIcon" src={FavoriteIcon} alt="favorite icon" />
-                            </div>
-
-                            <div className="Row2ColFloatBtn">
-                                <Image className="FloatBtnIcon" src={BucketIcon} alt="bucket icon" />
-                            </div>
-
-                            <div className="Row2ColFloatBtn">
-                                <Image className="FloatBtnIcon" src={ViewIcon} alt="view icon" />
-                            </div>
-
-                        </div>
-
-                        <CoverPage3Card
-                            row1Text="Certified Teacher"
-                            row2Text = "Every Client Matters"
-                        />
-                    </div>
-
-                </Carousel.Item>
-                
-            </Carousel>
-                
-        </div> : <div className="CoverPage3TextBoxRow2">
-            
+    return mediaQueries.mobile ? (
+      <div className="CoverPage3TextBoxRow2">
+        <Carousel>
+          <Carousel.Item>
             <div className="TextBoxRow2Col">
+              <Image className="Row2Col1Img" src={Row2Col1Img} alt="img" />
 
-                <Image className="Row2Col1Img" src={Row2Col1Img} alt="img" />  
+              <div className="Row2ColfloatImg">
+                <h6>Sale</h6>
+              </div>
 
-                <div className="Row2ColfloatImg">
-                    <h6>Sale</h6>
+              <div className="Row2ColFloatBtnBox">
+                <div className="Row2ColFloatBtn">
+                  <Image
+                    className="FloatBtnIcon"
+                    src={FavoriteIcon}
+                    alt="favorite icon"
+                  />
                 </div>
 
-                <div className="Row2ColFloatBtnBox">
-
-                    <div className="Row2ColFloatBtn">
-                        <Image className="FloatBtnIcon" src={FavoriteIcon} alt="favorite icon" />
-                    </div>
-
-                    <div className="Row2ColFloatBtn">
-                        <Image className="FloatBtnIcon" src={BucketIcon} alt="bucket icon" />
-                    </div>
-
-                    <div className="Row2ColFloatBtn">
-                        <Image className="FloatBtnIcon" src={ViewIcon} alt="view icon" />
-                    </div>
-
+                <div className="Row2ColFloatBtn">
+                  <Image
+                    className="FloatBtnIcon"
+                    src={BucketIcon}
+                    alt="bucket icon"
+                  />
                 </div>
 
-                <CoverPage3Card
-                    row1Text="Expert instruction"
-                    row2Text = "Our Experts Teacher"
-                />
+                <div className="Row2ColFloatBtn">
+                  <Image
+                    className="FloatBtnIcon"
+                    src={ViewIcon}
+                    alt="view icon"
+                  />
+                </div>
+              </div>
+
+              <CoverPage3Card
+                row1Text="Expert instruction"
+                row2Text="Our Experts Teacher"
+              />
+            </div>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className="TextBoxRow2Col">
+              <Image className="Row2Col1Img" src={Row2Col2Img} alt="img" />
+
+              <div className="Row2ColfloatImg">
+                <h6>Sale</h6>
+              </div>
+
+              <div className="Row2ColFloatBtnBox">
+                <div className="Row2ColFloatBtn">
+                  <Image
+                    className="FloatBtnIcon"
+                    src={FavoriteIcon}
+                    alt="favorite icon"
+                  />
+                </div>
+
+                <div className="Row2ColFloatBtn">
+                  <Image
+                    className="FloatBtnIcon"
+                    src={BucketIcon}
+                    alt="bucket icon"
+                  />
+                </div>
+
+                <div className="Row2ColFloatBtn">
+                  <Image
+                    className="FloatBtnIcon"
+                    src={ViewIcon}
+                    alt="view icon"
+                  />
+                </div>
+              </div>
+
+              <CoverPage3Card
+                row1Text="2,760 Online Courses"
+                row2Text="Get Quality Education"
+              />
+            </div>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className="TextBoxRow2Col">
+              <Image className="Row2Col1Img" src={Row2Col3Img} alt="img" />
+
+              <div className="Row2ColfloatImg">
+                <h6>Sale</h6>
+              </div>
+
+              <div className="Row2ColFloatBtnBox">
+                <div className="Row2ColFloatBtn">
+                  <Image
+                    className="FloatBtnIcon"
+                    src={FavoriteIcon}
+                    alt="favorite icon"
+                  />
+                </div>
+
+                <div className="Row2ColFloatBtn">
+                  <Image
+                    className="FloatBtnIcon"
+                    src={BucketIcon}
+                    alt="bucket icon"
+                  />
+                </div>
+
+                <div className="Row2ColFloatBtn">
+                  <Image
+                    className="FloatBtnIcon"
+                    src={ViewIcon}
+                    alt="view icon"
+                  />
+                </div>
+              </div>
+
+              <CoverPage3Card
+                row1Text="Training Courses"
+                row2Text="Our Popular Courses"
+              />
+            </div>
+          </Carousel.Item>
+
+          <Carousel.Item>
+            <div className="TextBoxRow2Col">
+              <Image className="Row2Col1Img" src={Row2Col4Img} alt="img" />
+
+              <div className="Row2ColfloatImg">
+                <h6>Sale</h6>
+              </div>
+
+              <div className="Row2ColFloatBtnBox">
+                <div className="Row2ColFloatBtn">
+                  <Image
+                    className="FloatBtnIcon"
+                    src={FavoriteIcon}
+                    alt="favorite icon"
+                  />
+                </div>
+
+                <div className="Row2ColFloatBtn">
+                  <Image
+                    className="FloatBtnIcon"
+                    src={BucketIcon}
+                    alt="bucket icon"
+                  />
+                </div>
+
+                <div className="Row2ColFloatBtn">
+                  <Image
+                    className="FloatBtnIcon"
+                    src={ViewIcon}
+                    alt="view icon"
+                  />
+                </div>
+              </div>
+
+              <CoverPage3Card
+                row1Text="Certified Teacher"
+                row2Text="Every Client Matters"
+              />
+            </div>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    ) : (
+      <div className="CoverPage3TextBoxRow2">
+        <div className="TextBoxRow2Col">
+          <Image className="Row2Col1Img" src={Row2Col1Img} alt="img" />
+
+          <div className="Row2ColfloatImg">
+            <h6>Sale</h6>
+          </div>
+
+          <div className="Row2ColFloatBtnBox">
+            <div className="Row2ColFloatBtn">
+              <Image
+                className="FloatBtnIcon"
+                src={FavoriteIcon}
+                alt="favorite icon"
+              />
             </div>
 
-            <div className="TextBoxRow2Col">
-                <Image className="Row2Col1Img" src={Row2Col2Img} alt="img" />
-                
-                <div className="Row2ColfloatImg">
-                    <h6>Sale</h6>
-                </div>
-
-                <div className="Row2ColFloatBtnBox">
-
-                    <div className="Row2ColFloatBtn">
-                        <Image className="FloatBtnIcon" src={FavoriteIcon} alt="favorite icon" />
-                    </div>
-
-                    <div className="Row2ColFloatBtn">
-                        <Image className="FloatBtnIcon" src={BucketIcon} alt="bucket icon" />
-                    </div>
-
-                    <div className="Row2ColFloatBtn">
-                        <Image className="FloatBtnIcon" src={ViewIcon} alt="view icon" />
-                    </div>
-
-                </div>
-
-                <CoverPage3Card
-                    row1Text="27 OnlineCourses"
-                    row2Text = "Get Quality Education"
-                />
+            <div className="Row2ColFloatBtn">
+              <Image
+                className="FloatBtnIcon"
+                src={BucketIcon}
+                alt="bucket icon"
+              />
             </div>
 
-            <div className="TextBoxRow2Col">
-
-                <Image className="Row2Col1Img" src={Row2Col3Img} alt="img" />
-                
-                <div className="Row2ColfloatImg">
-                    <h6>Sale</h6>
-                </div>
-
-                <div className="Row2ColFloatBtnBox">
-
-                    <div className="Row2ColFloatBtn">
-                        <Image className="FloatBtnIcon" src={FavoriteIcon} alt="favorite icon" />
-                    </div>
-
-                    <div className="Row2ColFloatBtn">
-                        <Image className="FloatBtnIcon" src={BucketIcon} alt="bucket icon" />
-                    </div>
-
-                    <div className="Row2ColFloatBtn">
-                        <Image className="FloatBtnIcon" src={ViewIcon} alt="view icon" />
-                    </div>
-
-                </div>
-
-                <CoverPage3Card
-                    row1Text = "training Courses"
-                    row2Text = "Our Popular Courses"
-                />
+            <div className="Row2ColFloatBtn">
+              <Image className="FloatBtnIcon" src={ViewIcon} alt="view icon" />
             </div>
+          </div>
 
-            <div className="TextBoxRow2Col">
-                <Image className="Row2Col1Img" src={Row2Col4Img} alt="img" />
-                
-                <div className="Row2ColfloatImg">
-                    <h6>Sale</h6>
-                </div>
-
-                <div className="Row2ColFloatBtnBox">
-
-                    <div className="Row2ColFloatBtn">
-                        <Image className="FloatBtnIcon" src={FavoriteIcon} alt="favorite icon" />
-                    </div>
-
-                    <div className="Row2ColFloatBtn">
-                        <Image className="FloatBtnIcon" src={BucketIcon} alt="bucket icon" />
-                    </div>
-
-                    <div className="Row2ColFloatBtn">
-                        <Image className="FloatBtnIcon" src={ViewIcon} alt="view icon" />
-                    </div>
-
-                </div>
-
-                <CoverPage3Card
-                    row1Text="Certified Teacher"
-                    row2Text = "Every Client Matters"
-                />
-            </div>
-
+          <CoverPage3Card
+            row1Text="Expert instruction"
+            row2Text="Our Experts Teacher"
+          />
         </div>
+
+        <div className="TextBoxRow2Col">
+          <Image className="Row2Col1Img" src={Row2Col2Img} alt="img" />
+
+          <div className="Row2ColfloatImg">
+            <h6>Sale</h6>
+          </div>
+
+          <div className="Row2ColFloatBtnBox">
+            <div className="Row2ColFloatBtn">
+              <Image
+                className="FloatBtnIcon"
+                src={FavoriteIcon}
+                alt="favorite icon"
+              />
+            </div>
+
+            <div className="Row2ColFloatBtn">
+              <Image
+                className="FloatBtnIcon"
+                src={BucketIcon}
+                alt="bucket icon"
+              />
+            </div>
+
+            <div className="Row2ColFloatBtn">
+              <Image className="FloatBtnIcon" src={ViewIcon} alt="view icon" />
+            </div>
+          </div>
+
+          <CoverPage3Card
+            row1Text="27 OnlineCourses"
+            row2Text="Get Quality Education"
+          />
+        </div>
+
+        <div className="TextBoxRow2Col">
+          <Image className="Row2Col1Img" src={Row2Col3Img} alt="img" />
+
+          <div className="Row2ColfloatImg">
+            <h6>Sale</h6>
+          </div>
+
+          <div className="Row2ColFloatBtnBox">
+            <div className="Row2ColFloatBtn">
+              <Image
+                className="FloatBtnIcon"
+                src={FavoriteIcon}
+                alt="favorite icon"
+              />
+            </div>
+
+            <div className="Row2ColFloatBtn">
+              <Image
+                className="FloatBtnIcon"
+                src={BucketIcon}
+                alt="bucket icon"
+              />
+            </div>
+
+            <div className="Row2ColFloatBtn">
+              <Image className="FloatBtnIcon" src={ViewIcon} alt="view icon" />
+            </div>
+          </div>
+
+          <CoverPage3Card
+            row1Text="training Courses"
+            row2Text="Our Popular Courses"
+          />
+        </div>
+
+        <div className="TextBoxRow2Col">
+          <Image className="Row2Col1Img" src={Row2Col4Img} alt="img" />
+
+          <div className="Row2ColfloatImg">
+            <h6>Sale</h6>
+          </div>
+
+          <div className="Row2ColFloatBtnBox">
+            <div className="Row2ColFloatBtn">
+              <Image
+                className="FloatBtnIcon"
+                src={FavoriteIcon}
+                alt="favorite icon"
+              />
+            </div>
+
+            <div className="Row2ColFloatBtn">
+              <Image
+                className="FloatBtnIcon"
+                src={BucketIcon}
+                alt="bucket icon"
+              />
+            </div>
+
+            <div className="Row2ColFloatBtn">
+              <Image className="FloatBtnIcon" src={ViewIcon} alt="view icon" />
+            </div>
+          </div>
+
+          <CoverPage3Card
+            row1Text="Certified Teacher"
+            row2Text="Every Client Matters"
+          />
+        </div>
+      </div>
     );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,45 +3,53 @@ import BrandLogo from "./BrandLogo";
 import Menu from "./Menu";
 import RightNavSection from "./RightNavSection";
 import { Col, Container, Row } from "react-bootstrap";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar } from "react-bootstrap";
 import useMediaQueries from "media-queries-in-react";
 
 function NavBar() {
+  const mediaQueries = useMediaQueries({
+    mobile: "screen and (max-width: 480px)",
+  });
 
-    const mediaQueries = useMediaQueries({
-        mobile: "screen and (max-width: 480px)",
-    });
-
-    return (
-
-
-        mediaQueries.mobile ?
-
-        <Navbar collapseOnSelect expand="lg" >
-            <Navbar.Brand href="#home">
-                BrandName
-                </Navbar.Brand>
-                <Col className="NavBtnCol" > <RightNavSection /> </Col>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#features">Home</Nav.Link>
-                        <Nav.Link href="#pricing">Product</Nav.Link>
-                        <Nav.Link href="#features">Pricing</Nav.Link>
-                        <Nav.Link href="#pricing">Contact</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-         </Navbar> : <Container fluid id="MainNavBarContainer">
-            <Container id="NavBarContainer">
-                <Row >
-                    <Col sm={3} className="NavBrandLogoCol" > <BrandLogo /> </Col>
-                    <Col className="NavMenuCol" > <Menu /> </Col>
-                    <Col className="NavBtnCol" > <RightNavSection /> </Col>
-                </Row>
-            </Container>
-            </Container> 
-            
-    );
+  return mediaQueries.mobile ? (
+    <Navbar collapseOnSelect expand="lg">
+      <Navbar.Brand href="#home">BrandName</Navbar.Brand>
+      <Col className="NavBtnCol">
+        <RightNavSection />
+      </Col>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">Home</Nav.Link>
+          <Nav.Link href="#pricing">Product</Nav.Link>
+          <Nav.Link href="#features">Pricing</Nav.Link>
+          <Nav.Link href="#pricing">Contact</Nav.Link>
+          {/* <Col className="NavBtnCol">
+              <RightNavSection />
+            </Col> */}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  ) : (
+    <Container fluid id="MainNavBarContainer">
+      <Container id="NavBarContainer">
+        <Row>
+          <Col sm={3} className="NavBrandLogoCol">
+            {" "}
+            <BrandLogo />{" "}
+          </Col>
+          <Col className="NavMenuCol">
+            {" "}
+            <Menu />{" "}
+          </Col>
+          <Col className="NavBtnCol">
+            {" "}
+            <RightNavSection />{" "}
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
 } 
 
 export default NavBar;
